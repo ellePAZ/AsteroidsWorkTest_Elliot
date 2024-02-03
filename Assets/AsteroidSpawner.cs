4fd345d0c396901a8d6784cf33cc2ccd727878b6
Elliot Padazakos <elliot.padazakos@outlook.com>
2024-02-03 13:23:19 +0100

    Project setup

---
+++ Assets/AsteroidSpawner.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+namespace Spawning
+{
+    public class AsteroidSpawner : MonoBehaviour
+    {
+        [SerializeField] AssetReferenceGameObject _asteroid;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
